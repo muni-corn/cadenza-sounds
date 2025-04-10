51f191ff69d44f17e144f17110b12394a6b74a0f
municorn <municorn@musicaloft.com>
2025-04-10 01:31:19 -0600

    chore(flake): organize inputs

--- flake.nix
+++ flake.nix
@@ -2,14 +2,11 @@
   description = "The Musicaloft sound theme";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+  outputs = { nixpkgs, flake-utils, ... }:
     let allSystems =
       flake-utils.lib.eachDefaultSystem (system:
         let
