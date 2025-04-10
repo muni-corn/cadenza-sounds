6e00966704e09028d6877480f772e383402467d3
municorn <municorn@musicaloft.com>
2025-04-10 01:31:35 -0600

    style(flake): format file

--- flake.nix
+++ flake.nix
@@ -6,27 +6,35 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    let allSystems =
-      flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      allSystems = flake-utils.lib.eachDefaultSystem (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
 
-          package =
-            pkgs.stdenv.mkDerivation {
-              pname = "muse-sounds";
-              version = "0.0.1";
+          package = pkgs.stdenv.mkDerivation {
+            pname = "muse-sounds";
+            version = "0.0.1";
 
-              src = builtins.path { path = ./.; name = "muse-sounds"; };
-
-              buildInputs = [ ];
-
-              configurePhase = "mkdir -pv $out/share/sounds/";
-              dontBuild = true;
-              installPhase = ''
-                cp -rv musicaloft/ $out/share/sounds/
-              '';
+            src = builtins.path {
+              path = ./.;
+              name = "muse-sounds";
             };
+
+            buildInputs = [ ];
+
+            configurePhase = "mkdir -pv $out/share/sounds/";
+            dontBuild = true;
+            installPhase = ''
+              cp -rv musicaloft/ $out/share/sounds/
+            '';
+          };
         in
         {
           packages = {
@@ -39,8 +47,7 @@
     {
       inherit (allSystems) packages defaultPackage;
       overlay = final: prev: {
-        muse-sounds =
-          allSystems.packages.${prev.system}.muse-sounds;
+        muse-sounds = allSystems.packages.${prev.system}.muse-sounds;
       };
     };
 }
